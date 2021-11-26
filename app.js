154dae0ed4cbb59ca35044b9378a6c52b6408ec5
Lammert Van der Pol <lammertvanderpol@MacBook-Pro-II-van-Lammert.local>
2021-11-26 22:41:58 +0100

    add app logic

--- app.js
+++ app.js
@@ -7,7 +7,14 @@ function setup(){
 function draw(){
 	background(255);	
 	line(0,height/2, width, height/2);	
+	line(20,height, 20, 0);
+	x= x -1;
+	if (x < 20) {
+		x = width,
+	}
+	y= x*0,2;
 
+	point(x, y);
 }
 
 
