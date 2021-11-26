ebfdb61869d5cd221453447ff15e681df0c01b16
Lammert Van der Pol <lammertvanderpol@MacBook-Pro-II-van-Lammert.local>
2021-11-26 22:34:41 +0100

    add app logic

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 function setup(){
-	createCanvas(400,400);
+	createCanvas(720,400);
+	stroke(0);
+	frameRate(30);
 }
 
 function draw(){
-	background(220);
+	background(255);	
+	line(0,height/2, width, height/2);	
 
 }
 
